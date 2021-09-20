3262a69db31553dfc6c5813fe9c4461b2d6e518c
Your Name <you@example.com>
2021-09-20 18:03:20 +0700

    add

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,46 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double celsius;
+        double fahrenheit;
+        int choice;
+
+        do {
+            System.out.println("Menu.");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter fahrenheit: ");
+                    fahrenheit = scanner.nextDouble();
+                    System.out.println("Fahrenheit to Celsius: " + fahrenheitToCelsius(fahrenheit));
+                    break;
+                case 2:
+                    System.out.println("Enter Celsisus: ");
+                    celsius = scanner.nextDouble();
+                    System.out.println("Celsius to Fahrenheit: " + celsiusToFahrenheit(celsius));
+                    break;
+                case 3:
+                    System.exit(3);
+                    break;
+            }
+        } while (choice != 0);
+    }
+    public static double celsiusToFahrenheit(double celsius) {
+        double fahrenheit = (9.0 / 5) * celsius + 32;
+        return fahrenheit;
+    }
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+        return celsius;
+    }
+}
